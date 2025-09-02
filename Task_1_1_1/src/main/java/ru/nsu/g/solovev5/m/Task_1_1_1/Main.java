e6ce82306e563fceede3db0c9cf2eec414d54e63
Matvey Solovev <m.solovev5@g.nsu.ru>
2025-09-02 22:03:26 +0700

    add basic solution

--- Task_1_1_1/src/main/java/ru/nsu/g/solovev5/m/Task_1_1_1/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/g/solovev5/m/Task_1_1_1/Main.java
@@ -1,7 +1,67 @@
 package ru.nsu.g.solovev5.m.Task_1_1_1;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        var array = new int[]{1, 2, 3, 10, 5, 6, 8, 11, 9, 16};
+        heapsort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void heapsort(int[] array) {
+        for (var i = 0; i < array.length; i++) {
+            siftUp(array, i);
+        }
+
+        for (var i = array.length; i > 0; i--) {
+            extractHead(array, i);
+        }
+    }
+
+    private static void extractHead(int[] array, final int heapLength) {
+        swap(array, 0, heapLength - 1);
+
+        siftDown(array, 0, heapLength - 1);
+    }
+
+    private static void siftUp(int[] array, final int position) {
+        if (position <= 0) return;
+
+        final var parentPosition = (position - 1) / 2;
+
+        if (compare(array[position], array[parentPosition])) {
+            swap(array, position, parentPosition);
+
+            siftUp(array, parentPosition);
+        }
+    }
+
+    private static void siftDown(int[] array, final int position, final int heapLength) {
+        final var leftChild = position * 2 + 1;
+        final var rightChild = position * 2 + 2;
+
+        if (leftChild >= heapLength) return;
+
+        var childToSwap = rightChild;
+        if (rightChild >= heapLength || compare(array[leftChild], array[rightChild])) {
+            childToSwap = leftChild;
+        }
+
+        if (compare(array[childToSwap], array[position])) {
+            swap(array, position, childToSwap);
+
+            siftDown(array, childToSwap, heapLength);
+        }
+    }
+
+    private static boolean compare(final int newRoot, final int root) {
+        return newRoot > root;
+    }
+
+    private static void swap(int[] array, final int i, final int j) {
+        final var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
---
+++ Task_1_1_1/src/test/java/ru/nsu/g/solovev5/m/Task_1_1_1/MainTest.java
@@ -0,0 +1,31 @@
+package ru.nsu.g.solovev5.m.Task_1_1_1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void heapsort() {
+        var array = new int[]{1, 2, 3, 10, 5, 6, 8, 11, 9, 16};
+        Main.heapsort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 5, 6, 8, 9, 10, 11, 16}, array);
+
+        var emptyArray = new int[0];
+        Main.heapsort(emptyArray);
+        assertArrayEquals(new int[0], emptyArray);
+
+        var ascendOrderedArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Main.heapsort(ascendOrderedArray);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, ascendOrderedArray);
+
+        var descendOrderedArray = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        Main.heapsort(descendOrderedArray);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, descendOrderedArray);
+
+        var duplicates = new int[]{1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 3, 1, 1, 1};
+        Main.heapsort(duplicates);
+        assertArrayEquals(new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3}, duplicates);
+    }
+}
