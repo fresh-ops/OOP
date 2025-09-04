e255bae25be0db60972cd9457f4ca035f542f925
Matvey Solovev <m.solovev5@g.nsu.ru>
2025-09-04 20:03:51 +0700

    separate tests

--- Task_1_1_1/src/test/java/ru/nsu/g/solovev5/m/task111/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/g/solovev5/m/task111/MainTest.java
@@ -7,30 +7,49 @@ import org.junit.jupiter.api.Test;
 class MainTest {
 
     @Test
-    void heapsort() {
+    void checkDefaultArray() {
         var array = new int[]{1, 2, 3, 10, 5, 6, 8, 11, 9, 16};
         Main.heapsort(array);
         assertArrayEquals(new int[]{1, 2, 3, 5, 6, 8, 9, 10, 11, 16}, array);
+    }
 
+    @Test
+    void checkSingleElement() {
+        var singleElementArray = new int[]{1};
+        Main.heapsort(singleElementArray);
+        assertArrayEquals(new int[]{1}, singleElementArray);
+    }
+
+    @Test
+    void checkEmptyArray() {
         var emptyArray = new int[0];
         Main.heapsort(emptyArray);
         assertArrayEquals(new int[0], emptyArray);
+    }
 
+    @Test
+    void checkAscendOrder() {
         var ascendOrderedArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         Main.heapsort(ascendOrderedArray);
         assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, ascendOrderedArray);
+    }
 
+    @Test
+    void checkDescendOrder() {
         var descendOrderedArray = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
         Main.heapsort(descendOrderedArray);
         assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, descendOrderedArray);
+    }
 
+    @Test
+    void checkDuplicates() {
         var duplicates = new int[]{1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 3, 1, 1, 1};
         Main.heapsort(duplicates);
         assertArrayEquals(new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3}, duplicates);
     }
 
     @Test
-    void main() {
+    void checkMain() {
         Main.main(new String[]{});
     }
 }
