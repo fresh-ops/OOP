f5f7c72f626c6585cfe570e2c669e620bb2a19f4
Matvey Solovev <m.solovev5@g.nsu.ru>
2025-09-11 10:14:45 +0700

    extend the Card class with isHidden field. Add javadoc

--- Task_1_1_2/src/main/java/ru/nsu/g/solovev5/m/task112/cards/Card.java
+++ Task_1_1_2/src/main/java/ru/nsu/g/solovev5/m/task112/cards/Card.java
@@ -1,8 +1,59 @@
 package ru.nsu.g.solovev5.m.task112.cards;
 
-public record Card(CardSuit suit, CardRank rank) {
+
+/**
+ * Represents a playing card. While a Card is immutable, it provides methods to
+ * hide and unhide the card via creating a new instance.
+ *
+ * @param suit a card suit
+ * @param rank a card rank for calculating the cards cost
+ * @param isHidden defines whether the card is hidden
+ */
+public record Card(CardSuit suit, CardRank rank, boolean isHidden) {
+   public Card(CardSuit suit, CardRank rank) {
+       this(suit, rank, false);
+   }
+
+    /**
+     * Hides this card via making a new instance with {@code true}
+     * to {@link #isHidden}.
+     *
+     * @return a new hidden card with equal suit and rank
+     */
+    public Card hide() {
+        return new Card(suit, rank, true);
+    }
+
+    /**
+     * Unhides this card via making a new instance with {@code false}
+     * to {@link #isHidden}.
+     *
+     * @return a new normal card with equal suit and rank
+     */
+    public Card unhide() {
+        return new Card(suit, rank, false);
+    }
+
+    /**
+     * Calculates a cost of this card.
+     *
+     * @return the default cost of the card of such rank if
+     * this card is not hidden, {@code 0} otherwise
+     */
+    public int cost() {
+        if (isHidden) {
+            return 0;
+        }
+
+        return rank.getCost();
+    }
+
     @Override
     public String toString() {
+        if (isHidden) {
+            return "<СКРЫТАЯ КАРТА>";
+        }
+
         return String.format("%s %s", rank, suit);
     }
 }
--- Task_1_1_2/src/test/java/ru/nsu/g/solovev5/m/task112/cards/CardTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/g/solovev5/m/task112/cards/CardTest.java
@@ -5,6 +5,46 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
+    @Test
+    void checkDefaultConstructorHiddenness() {
+        var card = new Card(CardSuit.SPADES, CardRank.SIX);
+
+        assertFalse(card.isHidden());
+    }
+
+    @Test
+    void checkUnhiddenCard() {
+        var card = new Card(CardSuit.CLUBS, CardRank.THREE, false);
+
+        assertFalse(card.isHidden());
+    }
+
+    @Test
+    void checkHiddenCard() {
+        var hiddenCard = new Card(CardSuit.DIAMONDS, CardRank.SEVEN, true);
+
+        assertTrue(hiddenCard.isHidden());
+    }
+
+    @Test
+    void checkHide() {
+        var card = new Card(CardSuit.HEARTS, CardRank.ACE);
+        var hiddenCard = card.hide();
+
+        assertTrue(hiddenCard.isHidden());
+        assertEquals(card.suit(), hiddenCard.suit());
+        assertEquals(card.rank(), hiddenCard.rank());
+    }
+
+    @Test
+    void checkUnhide() {
+        var hiddenCard = new Card(CardSuit.DIAMONDS, CardRank.FIVE, true);
+        var card = hiddenCard.unhide();
+
+        assertFalse(card.isHidden());
+        assertEquals(hiddenCard.suit(), card.suit());
+        assertEquals(hiddenCard.rank(), card.rank());
+    }
 
     @Test
     void checkStringRepresentation() {
@@ -13,14 +53,34 @@ class CardTest {
     }
 
     @Test
-    void suit() {
+    void checkHiddenStringRepresentation() {
+        var card = new Card(CardSuit.CLUBS, CardRank.EIGHT, true);
+        assertEquals("<СКРЫТАЯ КАРТА>", card.toString());
+    }
+
+    @Test
+    void checkSuit() {
         var card = new Card(CardSuit.HEARTS, CardRank.ACE);
         assertEquals(CardSuit.HEARTS, card.suit());
     }
 
     @Test
-    void rank() {
+    void checkRank() {
         var card = new Card(CardSuit.SPADES, CardRank.QUEEN);
         assertEquals(CardRank.QUEEN, card.rank());
+    }
+
+    @Test
+    void checkCost() {
+        var card = new Card(CardSuit.HEARTS, CardRank.FIVE);
+
+        assertEquals(CardRank.FIVE.getCost(), card.cost());
+    }
+
+    @Test
+    void checkHiddenCardCost() {
+        var hiddenCard = new Card(CardSuit.DIAMONDS, CardRank.JACK, true);
+
+        assertEquals(0, hiddenCard.cost());
     }
 }
