05f9270b4befe6bcceced482b3107669a4b887ad
Matvey Solovev <m.solovev5@g.nsu.ru>
2025-09-13 21:06:54 +0700

    add Hand class

---
+++ Task_1_1_2/src/test/java/ru/nsu/g/solovev5/m/task112/cards/HandTest.java
@@ -0,0 +1,94 @@
+package ru.nsu.g.solovev5.m.task112.cards;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandTest {
+    Hand hand;
+    Card card, ace, jack, hiddenCard;
+
+    @BeforeEach
+    void setUp() {
+        hand = new Hand();
+        card = new Card(CardSuit.DIAMONDS, CardRank.FIVE);
+        ace = new Card(CardSuit.HEARTS, CardRank.ACE);
+        jack = new Card(CardSuit.CLUBS, CardRank.JACK);
+        hiddenCard = new Card(CardSuit.SPADES, CardRank.EIGHT, true);
+    }
+
+    @Test
+    void checkInitState() {
+        assertEquals(0, hand.getCost());
+    }
+
+    @Test
+    void checkPuttingCard() {
+        hand.put(card);
+        hand.put(jack);
+
+        assertEquals(card.cost() + jack.cost(), hand.getCost());
+    }
+
+    @Test
+    void checkPuttingHiddenCard() {
+        hand.putHidden(card);
+        hand.put(hiddenCard);
+        hand.putHidden(ace);
+
+        assertEquals(0, hand.getCost());
+    }
+
+    @Test
+    void checkAcesHandling() {
+        hand.put(ace);
+        hand.put(ace);
+        hand.put(ace);
+        hand.put(ace);
+
+        assertEquals(14, hand.getCost());
+    }
+
+    @Test
+    void checkHidingCard() {
+        hand.put(ace);
+        hand.put(jack);
+
+        assertEquals(21, hand.getCost());
+
+        hand.hide(0);
+        assertEquals(10, hand.getCost());
+    }
+
+    @Test
+    void checkUnhidingCard() {
+        hand.putHidden(ace);
+        hand.put(hiddenCard);
+
+        assertEquals(0, hand.getCost());
+
+        hand.unhide(1);
+        assertEquals(hiddenCard.unhide().cost(), hand.getCost());
+    }
+
+    @Test
+    void checkUnhidingAllCards() {
+        hand.putHidden(ace);
+        hand.put(hiddenCard);
+
+        assertEquals(0, hand.getCost());
+
+        hand.unhideAll();
+        assertEquals(hiddenCard.unhide().cost() + ace.cost(), hand.getCost());
+    }
+
+    @Test
+    void checkDiscard() {
+        hand.put(ace);
+        hand.put(jack);
+
+        hand.discard();
+        assertEquals(0, hand.getCost());
+    }
+}
