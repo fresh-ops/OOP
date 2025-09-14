5d497d18d10e65c4337b1caae1552b520ea593a2
Matvey Solovev <m.solovev5@g.nsu.ru>
2025-09-14 18:35:58 +0700

    remove spaces in enum

--- Task_1_1_2/src/main/java/ru/nsu/g/solovev5/m/task112/cards/CardSuit.java
+++ Task_1_1_2/src/main/java/ru/nsu/g/solovev5/m/task112/cards/CardSuit.java
@@ -1,10 +1,10 @@
 package ru.nsu.g.solovev5.m.task112.cards;
 
 public enum CardSuit {
-    CLUBS ("ТРЕФЫ"),
-    DIAMONDS ("БУБИ"),
-    HEARTS ("ЧЕРВИ"),
-    SPADES ("ПИКИ");
+    CLUBS("ТРЕФЫ"),
+    DIAMONDS("БУБИ"),
+    HEARTS("ЧЕРВИ"),
+    SPADES("ПИКИ");
 
     private final String name;
 
