916fcfae88f9e536e60f1e2c10c6f267942980ae
Matvey Solovev <m.solovev5@g.nsu.ru>
2025-09-15 09:58:31 +0700

    add game test

--- Task_1_1_2/src/test/java/ru/nsu/g/solovev5/m/task112/BlackjackGameTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/g/solovev5/m/task112/BlackjackGameTest.java
@@ -7,10 +7,13 @@ import org.junit.jupiter.api.Test;
 class BlackjackGameTest {
     @Test
     void checkGame() {
-        var inputTest = "test 0 0".getBytes(StandardCharsets.UTF_8);
-        var simulatedInput = new ByteArrayInputStream(inputTest);
+        for (var i = 0; i < 1_000; i++) {
+            var inputTest = "test 1 1 0 0 1 wrong 1 0 0 4 3 1 1 0 0 0 0 0 0"
+                    .getBytes(StandardCharsets.UTF_8);
+            var simulatedInput = new ByteArrayInputStream(inputTest);
 
-        System.setIn(simulatedInput);
-        BlackjackGame.main(new String[0]);
+            System.setIn(simulatedInput);
+            BlackjackGame.main(new String[0]);
+        }
     }
 }
