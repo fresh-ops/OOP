2f5806d33697dc37974a595f2885db45184d6736
Matvey Solovyov <83802608+fresh-ops@users.noreply.github.com>
2025-12-01 10:01:46 +0700

    Task_1_2_2 (#6)
    * add basic HashTable
    
    * add method to check if a key is in a table
    
    * generalize HashTable interface
    
    * implement Iterable interface in HashTable
    
    * add equals method to HashTable
    
    * add HashTable size decreasing
    
    * add HashTable toString method
    
    * add HashTable tests
    
    * add more tests
    
    * remove suppress statements

---
+++ Task_1_2_2/src/main/java/ru/nsu/g/solovev5/m/task122/HashTable.java
@@ -0,0 +1,386 @@
+package ru.nsu.g.solovev5.m.task122;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+/**
+ * A hash table class. Uses hash-bucket approach to handle hash collisions.
+ *
+ * @param <K> a key type
+ * @param <V> a value type
+ */
+public class HashTable<K, V> implements Iterable<HashTable.Entry<K, V>> {
+    private static final int DEFAULT_CAPACITY = 8;
+    private static final double INCREASE_LOAD_FACTOR_THRESHOLD = 0.75;
+    private static final double DECREASE_LOAD_FACTOR_THRESHOLD = 0.25;
+
+    private static final int HASH_MIXER_A = 0x85EBCA6B;
+    private static final int HASH_MIXER_B = 0xC2B2AE35;
+
+    private int capacity;
+    private int size;
+    private int modCount;
+    private Entry<K, V>[] buckets;
+
+    /**
+     * Creates a new HashTable with default capacity.
+     */
+    public HashTable() {
+        capacity = DEFAULT_CAPACITY;
+        size = 0;
+        modCount = 0;
+        buckets = new Entry[capacity];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof HashTable<?, ?> hashTable) || size != hashTable.size) {
+            return false;
+        }
+
+        for (var entry : hashTable) {
+            if (!has(entry.key) || !Objects.equals(get(entry.key), entry.value)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns a number of elements in this table.
+     *
+     * @return a number of elements in this table
+     */
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public int hashCode() {
+        var hash = 0;
+
+        for (var entry : this) {
+            hash += entry.hashCode();
+        }
+
+        return hash;
+    }
+
+    @Override
+    public Iterator<Entry<K, V>> iterator() {
+        return new HashTableIterator();
+    }
+
+    /**
+     * Maps a value to given key. If this key already have a mapping,
+     * it replaces old value with a new one and returns it.
+     *
+     * @param key   a key to map
+     * @param value a value to hold
+     * @return a prior value, or null if no such was
+     */
+    public V put(K key, V value) {
+        var index = indexOf(key);
+        var bucket = buckets[index];
+
+        while (bucket != null) {
+            if (Objects.equals(key, bucket.key)) {
+                return bucket.setValue(value);
+            }
+
+            bucket = bucket.next;
+        }
+
+        modCount++;
+        if (++size > capacity * INCREASE_LOAD_FACTOR_THRESHOLD) {
+            resize(true);
+            index = indexOf(key);
+        }
+
+        addEntry(key, value, index);
+        return null;
+    }
+
+    /**
+     * Provides a value mapped to a given key.
+     *
+     * @param key a mapped key
+     * @return a mapped value, or null if no such exists
+     */
+    public V get(Object key) {
+        var bucket = buckets[indexOf(key)];
+
+        while (bucket != null) {
+            if (Objects.equals(key, bucket.key)) {
+                return bucket.value;
+            }
+
+            bucket = bucket.next;
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns {@code true} if this table contains given key.
+     *
+     * @param key a key to check
+     * @return {@code true} if this table contains given key, {@code false} otherwise
+     */
+    public boolean has(Object key) {
+        var bucket = buckets[indexOf(key)];
+
+        while (bucket != null) {
+            if (Objects.equals(key, bucket.key)) {
+                return true;
+            }
+
+            bucket = bucket.next;
+        }
+
+        return false;
+    }
+
+    /**
+     * Removes a value mapped to a given key.
+     *
+     * @param key a mapped key
+     * @return a removed value, or null if nothing found
+     */
+    public V remove(Object key) {
+        modCount++;
+        var index = indexOf(key);
+        var bucket = buckets[index];
+
+        if (bucket == null) {
+            return null;
+        } else if (Objects.equals(key, bucket.key)) {
+            buckets[index] = bucket.next;
+
+            if (--size < capacity * DECREASE_LOAD_FACTOR_THRESHOLD) {
+                resize(false);
+            }
+            bucket.next = null;
+            return bucket.value;
+        }
+
+        while (bucket.next != null) {
+            if (Objects.equals(key, bucket.next.key)) {
+                var removing = bucket.next;
+                bucket.next = removing.next;
+
+                if (--size < capacity * DECREASE_LOAD_FACTOR_THRESHOLD) {
+                    resize(false);
+                }
+                removing.next = null;
+                return removing.value;
+            }
+
+            bucket = bucket.next;
+        }
+
+        return null;
+    }
+
+    /**
+     * Adds a new entry into the table.
+     *
+     * @param key   an entry key
+     * @param value an entry value
+     * @param index an index to put in
+     */
+    private void addEntry(K key, V value, int index) {
+        var entry = new Entry<>(key, value);
+        entry.next = buckets[index];
+        buckets[index] = entry;
+    }
+
+    /**
+     * Changes this table capacity and rehashes entries.
+     *
+     * @param increaseSize defines whether to increase this table capacity or to decrease
+     */
+    private void resize(boolean increaseSize) {
+        if (increaseSize && capacity << 1 > 0) {
+            capacity <<= 1;
+        } else if (capacity > DEFAULT_CAPACITY) {
+            capacity >>= 1;
+        } else {
+            return;
+        }
+
+        rehashAll();
+    }
+
+    /**
+     * Rehashes all entries in this table to fit new capacity.
+     */
+    private void rehashAll() {
+        var oldBuckets = buckets;
+        buckets = new Entry[capacity];
+
+        for (Entry<K, V> bucket : oldBuckets) {
+            while (bucket != null) {
+                var index = indexOf(bucket.key);
+                var next = bucket.next;
+                bucket.next = buckets[index];
+                buckets[index] = bucket;
+
+                bucket = next;
+            }
+        }
+    }
+
+    /**
+     * Calculates a hash for a given key.
+     *
+     * @param key a key to hash
+     * @return hashed key that can be used as an index
+     */
+    private int indexOf(Object key) {
+        if (key == null) {
+            return 0;
+        }
+
+        var hash = key.hashCode();
+        hash ^= hash >>> 16;
+        hash *= HASH_MIXER_A;
+        hash ^= hash >>> 13;
+        hash *= HASH_MIXER_B;
+        hash ^= hash >>> 16;
+
+        return hash & (capacity - 1);
+    }
+
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "{}";
+        }
+
+        var sb = new StringBuilder().append('{');
+        for (var entry : this) {
+            sb.append(entry).append(", ");
+        }
+
+        var length = sb.length();
+        sb.delete(length - 2, length);
+        sb.append('}');
+
+        return sb.toString();
+    }
+
+    /**
+     * A class to represent an entry of a table.
+     *
+     * @param <K> a key type
+     * @param <V> a value type
+     */
+    public static class Entry<K, V> {
+        private Entry<K, V> next;
+        private final K key;
+        private V value;
+
+        /**
+         * Creates a new table entry.
+         *
+         * @param key   an entry key
+         * @param value an entry value
+         */
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Entry<?, ?> entry)) {
+                return false;
+            }
+
+            return Objects.equals(key, entry.key) && Objects.equals(value, entry.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key, value);
+        }
+
+        /**
+         * Returns an entry key.
+         *
+         * @return an entry key
+         */
+        public K getKey() {
+            return key;
+        }
+
+        /**
+         * Returns an entry value.
+         *
+         * @return an entry value
+         */
+        public V getValue() {
+            return value;
+        }
+
+        /**
+         * Sets the entry value.
+         *
+         * @param newValue a new entry value
+         * @return an old entry value
+         */
+        public V setValue(V newValue) {
+            var oldValue = value;
+            value = newValue;
+
+            return oldValue;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s=%s", key, value);
+        }
+    }
+
+    private class HashTableIterator implements Iterator<Entry<K, V>> {
+        private int remaining;
+        private final int knownMod;
+        private int lastIndex;
+        private Entry<K, V> lastBucket;
+
+        HashTableIterator() {
+            this.remaining = size;
+            this.lastIndex = -1;
+            this.knownMod = modCount;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return remaining > 0;
+        }
+
+        @Override
+        public Entry<K, V> next() {
+            if (knownMod != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (remaining <= 0) {
+                throw new NoSuchElementException();
+            }
+            remaining--;
+
+            if (lastBucket != null) {
+                lastBucket = lastBucket.next;
+            }
+            while (lastBucket == null) {
+                lastBucket = buckets[++lastIndex];
+            }
+
+            return lastBucket;
+        }
+    }
+}
---
+++ Task_1_2_2/src/test/java/ru/nsu/g/solovev5/m/task122/HashTableTest.java
@@ -0,0 +1,294 @@
+package ru.nsu.g.solovev5.m.task122;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class HashTableTest {
+    HashTable<String, Number> table;
+
+    @BeforeEach
+    void setUp() {
+        table = new HashTable<>();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void checkPuttingElements(String message,
+                              List<HashTable.Entry<String, Number>> entries) {
+        assertEquals(
+            0, table.size(),
+            message + ". An empty table should have a zero size"
+        );
+
+        for (var entry : entries) {
+            assertFalse(
+                table.has(entry.getKey()),
+                message + ". A table shouldn't contain an entry before adding"
+            );
+            assertNull(
+                table.get(entry.getKey()),
+                message + ". A not stated key should be associated with null"
+            );
+            assertDoesNotThrow(
+                () -> table.put(entry.getKey(), entry.getValue()),
+                message + ". Putting an entry shouldn't cause any exception"
+            );
+            assertTrue(
+                table.has(entry.getKey()),
+                message + ". A table should contain an entry after adding"
+            );
+        }
+
+        assertEquals(
+            entries.size(), table.size(),
+            message + ". A table size should be same as an entries count"
+        );
+
+        for (var entry : entries) {
+            assertTrue(
+                table.has(entry.getKey()),
+                message + ". A table should contain all entries after adding"
+            );
+            assertEquals(
+                entry.getValue(), table.get(entry.getKey()),
+                message + ". A containing value shouldn't change"
+            );
+        }
+    }
+
+    static Stream<Arguments> checkPuttingElements() {
+        var one = new HashTable.Entry<String, Number>("One", 1);
+        var two = new HashTable.Entry<String, Number>("Two", 2);
+        var three = new HashTable.Entry<String, Number>("Three", 3);
+
+        var nullKey = new HashTable.Entry<String, Number>(null, 0);
+        var nullValue = new HashTable.Entry<String, Number>("Null Value", null);
+
+        var manyEntries = new ArrayList<HashTable.Entry<String, Number>>();
+        for (var i = 0; i < 1000; i++) {
+            manyEntries.add(new HashTable.Entry<>(Integer.toString(i), i));
+        }
+
+        return Stream.of(
+            Arguments.of(
+                "Single item", List.of(one)
+            ),
+            Arguments.of(
+                "Multiple items", List.of(one, two, three)
+            ),
+            Arguments.of(
+                "Null fields", List.of(one, nullKey, two, nullValue)
+            ),
+            Arguments.of(
+                "Large elements number", manyEntries
+            )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void checkUpdatingElements(String message,
+                               List<HashTable.Entry<String, Number>> entries,
+                               List<HashTable.Entry<String, Number>> replacements) {
+        for (var entry : entries) {
+            table.put(entry.getKey(), entry.getValue());
+        }
+
+        for (var replacement : replacements) {
+            assertDoesNotThrow(
+                () -> table.put(replacement.getKey(), replacement.getValue()),
+                message + ". Replacing an element shouldn't cause any exception"
+            );
+            assertEquals(
+                replacement.getValue(), table.get(replacement.getKey()),
+                message + ". After replacing a new value should be presented"
+            );
+        }
+
+        assertEquals(
+            entries.size(), table.size(),
+            message + ". A table size should be same as before replacing"
+        );
+    }
+
+    static Stream<Arguments> checkUpdatingElements() {
+        var a1 = new HashTable.Entry<String, Number>("a", 1);
+        var a2 = new HashTable.Entry<String, Number>("a", 100);
+
+        var b1 = new HashTable.Entry<String, Number>("b", 2);
+        var b2 = new HashTable.Entry<String, Number>("b", 200);
+        var b3 = new HashTable.Entry<String, Number>("b", 20_000);
+
+        var c1 = new HashTable.Entry<String, Number>("c", 3);
+        var c2 = new HashTable.Entry<String, Number>("c", 34);
+        var c3 = new HashTable.Entry<String, Number>("c", 345);
+        var c4 = new HashTable.Entry<String, Number>("c", 3_456);
+
+        return Stream.of(
+            Arguments.of(
+                "A single replacement",
+                List.of(a1, b1, c1),
+                List.of(a1)
+            ),
+            Arguments.of(
+                "Multiple replacements of one element",
+                List.of(a1, b1, c1),
+                List.of(c2, c3, c4)
+            ),
+            Arguments.of(
+                "Multiple shuffled replacements",
+                List.of(a1, b1, c1),
+                List.of(b2, c2, a2, c3, c4, b3)
+            )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void checkRemovingElements(String message,
+                               List<HashTable.Entry<String, Number>> entries,
+                               List<HashTable.Entry<String, Number>> removings,
+                               int correctlyRemoved) {
+        for (var entry : entries) {
+            table.put(entry.getKey(), entry.getValue());
+        }
+
+        for (var removing : removings) {
+            assertDoesNotThrow(
+                () -> table.remove(removing.getKey()),
+                message + ". Removing an element shouldn't cause any exception"
+            );
+            assertFalse(
+                table.has(removing.getKey()),
+                message + ". After removing an element shouldn't be in the table"
+            );
+            assertNull(
+                table.get(removing.getKey()),
+                message + ". A removed element should null"
+            );
+        }
+
+        assertEquals(
+            entries.size() - correctlyRemoved, table.size(),
+            message + ". A removing an element should decrease the table size"
+        );
+    }
+
+    static Stream<Arguments> checkRemovingElements() {
+        var one = new HashTable.Entry<String, Number>("One", 1);
+        var two = new HashTable.Entry<String, Number>("Two", 2);
+        var three = new HashTable.Entry<String, Number>("Three", 3);
+
+        var manyEntries = new ArrayList<HashTable.Entry<String, Number>>();
+        for (var i = 0; i < 1000; i++) {
+            manyEntries.add(new HashTable.Entry<>(Integer.toString(i), i));
+        }
+
+        return Stream.of(
+            Arguments.of(
+                "Single removing", List.of(one, two, three),
+                List.of(one), 1
+            ),
+            Arguments.of(
+                "Removing all", List.of(one, two, three),
+                List.of(one, two, three), 3
+
+            ),
+            Arguments.of(
+                "Removing non-present element", List.of(one, three),
+                List.of(two), 0
+            ),
+            Arguments.of(
+                "Large elements number", manyEntries, manyEntries, manyEntries.size()
+            )
+        );
+    }
+
+    @Test
+    void checkEquals() {
+        var a = new HashTable.Entry<String, Number>("a", 1);
+        table.put(a.getKey(), a.getValue());
+        var b = new HashTable.Entry<String, Number>("b", 2);
+        table.put(b.getKey(), b.getValue());
+        var c = new HashTable.Entry<String, Number>("c", 3);
+        table.put(c.getKey(), c.getValue());
+        var d = new HashTable.Entry<String, Number>("d", 4);
+        table.put(d.getKey(), d.getValue());
+        var e = new HashTable.Entry<String, Number>("e", 5);
+        table.put(e.getKey(), e.getValue());
+
+        var other = new HashTable<String, Number>();
+        other.put(a.getKey(), 0);
+        other.put(b.getKey(), b.getValue());
+        other.put(c.getKey(), c.getValue());
+        other.put(d.getKey(), d.getValue());
+        other.put(e.getKey(), e.getValue());
+
+        assertNotEquals(
+            "HashTable", table,
+            "A table can be equal only to another table"
+        );
+        assertNotEquals(
+            table, other,
+            "Tables with different values at the same key are not equal"
+        );
+
+        table.remove(b.getKey());
+        table.remove(c.getKey());
+        other.remove(a.getKey());
+
+        assertNotEquals(
+            table, other,
+            "Tables with different keys are not equal"
+        );
+
+        table.remove(a.getKey());
+        other.remove(b.getKey());
+        other.remove(c.getKey());
+
+        assertEquals(
+            table, other,
+            "Tables with the same keys and mapped values are equal"
+        );
+
+        assertEquals(
+            table.hashCode(), other.hashCode(),
+            "Equal tables should have the same hash code"
+        );
+    }
+
+    @Test
+    void checkToString() {
+        var table = new HashTable<String, Number>();
+        assertEquals(
+            "{}", table.toString(),
+            "Empty table should be represented as {}"
+        );
+
+        table.put("a", 1);
+        assertEquals(
+            "{a=1}", table.toString(),
+            "Single element table"
+        );
+
+        table.put("b", 2);
+        var str = table.toString();
+        assertTrue(
+            str.equals("{a=1, b=2}") || str.equals("{b=2, a=1}"),
+            "Two element table"
+        );
+    }
+}
