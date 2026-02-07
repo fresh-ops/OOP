3f1d6af1d4f5bd05b09a12b683e0c76793e08bac
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-02-08 01:31:02 +0700

    fix: add missing space

--- Task_2_1_1/src/test/java/ru/nsu/g/solovev5/m/task211/primes/ParallelStreamPrimeCheckerTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/g/solovev5/m/task211/primes/ParallelStreamPrimeCheckerTest.java
@@ -3,7 +3,7 @@ package ru.nsu.g.solovev5.m.task211.primes;
 /**
  * Test for {@link ParallelStreamPrimeChecker}.
  */
-public class ParallelStreamPrimeCheckerTest extends PrimeCheckerImplementationTest{
+public class ParallelStreamPrimeCheckerTest extends PrimeCheckerImplementationTest {
     @Override
     public PrimeChecker createPrimeChecker() {
         return new ParallelStreamPrimeChecker();
