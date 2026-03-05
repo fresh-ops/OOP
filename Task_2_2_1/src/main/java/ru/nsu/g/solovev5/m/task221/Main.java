cea1d29d7b868b60a639a2c7b9d5f2d3af7aa44b
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-03-05 09:21:30 +0700

    init task 2 2 1

---
+++ Task_2_2_1/src/main/java/ru/nsu/g/solovev5/m/task221/Main.java
@@ -0,0 +1,15 @@
+package ru.nsu.g.solovev5.m.task221;
+
+/**
+ * The program starter.
+ */
+public class Main {
+    /**
+     * The program entry point.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
