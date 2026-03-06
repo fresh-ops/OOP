aa38f58233754e66dcd01148c3063ef143764659
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-03-07 00:48:55 +0700

    add baker interruption fallback

--- Task_2_2_1/src/main/java/ru/nsu/g/solovev5/m/task221/pizzeria/actors/Baker.java
+++ Task_2_2_1/src/main/java/ru/nsu/g/solovev5/m/task221/pizzeria/actors/Baker.java
@@ -1,22 +1,25 @@
 package ru.nsu.g.solovev5.m.task221.pizzeria.actors;
 
+import ru.nsu.g.solovev5.m.task221.pizzeria.orders.Order;
 import ru.nsu.g.solovev5.m.task221.pizzeria.orders.OrderQueue;
 import ru.nsu.g.solovev5.m.task221.pizzeria.warehouse.PizzaWarehouse;
 
 /**
  * This is a baker, he works three days without a salary.
  */
-public class Baker extends Thread {
+public class Baker implements Runnable {
     private final int cookingSpeed;
     private final OrderQueue orders;
     private final PizzaWarehouse warehouse;
+
+    private Order interruptedOrder;
 
     /**
      * Creates a new baker with the given cooking speed.
      *
      * @param cookingSpeed the speed of cooking a pizza
-     * @param orders the queue of orders
-     * @param warehouse the pizza warehouse
+     * @param orders       the queue of orders
+     * @param warehouse    the pizza warehouse
      */
     public Baker(int cookingSpeed, OrderQueue orders, PizzaWarehouse warehouse) {
         this.cookingSpeed = cookingSpeed;
@@ -26,28 +29,59 @@ public class Baker extends Thread {
 
     @Override
     public void run() {
-        while (!isInterrupted()) {
+        this.interruptedOrder = null;
+
+        Order order;
+        while (!Thread.currentThread().isInterrupted()) {
             try {
-                cook();
+                order = orders.take();
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
+                break;
+            }
+
+            cook(order);
+            order.promoteStatus();
+
+            try {
+                warehouse.put(order);
+            } catch (InterruptedException e) {
+                interruptedOrder = order;
+                Thread.currentThread().interrupt();
+                break;
             }
         }
     }
 
     /**
-     * Cooks the order from queue and puts it to the warehouse.
+     * Checks if this baker has interrupted order.
+     *
+     * @return {@code true} if there is an interrupted order, {@code false} otherwise
      */
-    private void cook() throws InterruptedException {
-        var order = orders.take();
+    public boolean hasInterruptedOrder() {
+        return interruptedOrder != null;
+    }
+
+    /**
+     * Returns the interrupted order.
+     *
+     * @return the interrupted order.
+     */
+    public Order getInterruptedOrder() {
+        return interruptedOrder;
+    }
+
+    /**
+     * Cooks the given order.
+     *
+     * @param order the order to cook
+     */
+    private void cook(Order order) {
         order.promoteStatus();
         var pizza = order.getPizza();
 
         while (!pizza.isCooked()) {
             pizza.cookFor(cookingSpeed);
         }
-
-        order.promoteStatus();
-        warehouse.put(order);
     }
 }
