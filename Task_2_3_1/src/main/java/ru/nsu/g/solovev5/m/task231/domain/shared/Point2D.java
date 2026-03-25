7e8c5e31f7d8b422760f2ea5fc63714668158660
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-03-25 10:25:41 +0700

    add: point class

---
+++ Task_2_3_1/src/main/java/ru/nsu/g/solovev5/m/task231/domain/shared/Point2D.java
@@ -0,0 +1,10 @@
+package ru.nsu.g.solovev5.m.task231.domain.shared;
+
+/**
+ * A pair of two Cartesian coordinates.
+ *
+ * @param x the horizontal coordinate
+ * @param y the vertical coordinate
+ */
+public record Point2D(int x, int y) {
+}
