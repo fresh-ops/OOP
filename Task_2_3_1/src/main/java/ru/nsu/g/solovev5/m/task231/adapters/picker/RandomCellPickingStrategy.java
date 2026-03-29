a80553d304a784ce89a460f6673f2f2da9b82530
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-03-29 18:46:53 +0700

    add: RandomCellPickingStrategy docs

--- Task_2_3_1/src/main/java/ru/nsu/g/solovev5/m/task231/adapters/picker/RandomCellPickingStrategy.java
+++ Task_2_3_1/src/main/java/ru/nsu/g/solovev5/m/task231/adapters/picker/RandomCellPickingStrategy.java
@@ -5,9 +5,15 @@ import java.util.Random;
 import ru.nsu.g.solovev5.m.task231.application.strategies.CellPickingStrategy;
 import ru.nsu.g.solovev5.m.task231.domain.valueobjects.Point2D;
 
+/**
+ * Picks a cell using random.
+ */
 public class RandomCellPickingStrategy implements CellPickingStrategy {
     private final Random random;
 
+    /**
+     * Creates a new RandomCellPickingStrategy.
+     */
     public RandomCellPickingStrategy() {
         this.random = new Random();
     }
