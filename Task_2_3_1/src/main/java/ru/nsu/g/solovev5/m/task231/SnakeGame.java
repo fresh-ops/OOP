73b54ef65f47b8bf98cc56c96972ae0be5da9c95
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-04-03 00:38:10 +0700

    deprecate: BoardController and GameScreen

--- Task_2_3_1/src/main/java/ru/nsu/g/solovev5/m/task231/SnakeGame.java
+++ Task_2_3_1/src/main/java/ru/nsu/g/solovev5/m/task231/SnakeGame.java
@@ -1,22 +1,10 @@
 package ru.nsu.g.solovev5.m.task231;
 
-import java.util.List;
-import java.util.Objects;
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import ru.nsu.g.solovev5.m.task231.adapters.keyboard.KeyboardMovementStrategy;
-import ru.nsu.g.solovev5.m.task231.adapters.picker.RandomCellPickingStrategy;
-import ru.nsu.g.solovev5.m.task231.application.GameSession;
-import ru.nsu.g.solovev5.m.task231.application.config.GameSessionConfig;
-import ru.nsu.g.solovev5.m.task231.application.config.PlayerConfig;
-import ru.nsu.g.solovev5.m.task231.domain.valueobjects.FoodType;
-import ru.nsu.g.solovev5.m.task231.domain.valueobjects.Point2D;
-import ru.nsu.g.solovev5.m.task231.presentation.gamescreen.GameScreenController;
+import ru.nsu.g.solovev5.m.task231.presentation.drawer.GridDrawer;
 
 /**
  * The main program class.
@@ -35,50 +23,12 @@ public class SnakeGame extends Application {
     public void start(Stage stage) throws Exception {
         stage.setTitle("Snake Game");
 
-        var gameScreenResource = getClass().getResource("presentation/gamescreen/GameScreen.fxml");
-        Objects.requireNonNull(gameScreenResource);
-        var gameScreenLoader = new FXMLLoader(gameScreenResource);
-        Node gameScreen = gameScreenLoader.load();
-        GameScreenController controller = gameScreenLoader.getController();
-
-
+        var drawer = new GridDrawer(12, 15);
         var pane = new BorderPane();
-        pane.setCenter(gameScreen);
+        pane.setCenter(drawer);
+
         var scene = new Scene(pane, 640, 480);
 
-        var movement = new KeyboardMovementStrategy(5);
-        scene.setOnKeyPressed(movement::onKeyPressed);
-
-        var session = new GameSession(
-            new GameSessionConfig(
-                12, 12, 5,
-                List.of(new PlayerConfig(new Point2D(0, 0), movement)),
-                new RandomCellPickingStrategy(),
-                () -> FoodType.NORMAL
-            )
-        );
-
-        var gameLoop = new AnimationTimer() {
-            private long lastUpdate;
-
-            @Override
-            public void handle(long now) {
-                if (now - lastUpdate <= 25_0000_000) {
-                    return;
-                }
-                session.tick();
-                lastUpdate = now;
-            }
-        };
-        var animationLoop = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                controller.render(session.getFrozenState());
-            }
-        };
-
-        gameLoop.start();
-        animationLoop.start();
 
         stage.setScene(scene);
         stage.show();
