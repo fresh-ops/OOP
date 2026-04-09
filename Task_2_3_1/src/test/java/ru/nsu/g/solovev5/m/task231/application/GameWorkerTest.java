8db826c484ea0796c3b2755e78f1278bf8646279
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-04-09 09:57:42 +0700

    add: worker tests

---
+++ Task_2_3_1/src/test/java/ru/nsu/g/solovev5/m/task231/application/GameWorkerTest.java
@@ -0,0 +1,87 @@
+package ru.nsu.g.solovev5.m.task231.application;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.nsu.g.solovev5.m.task231.application.config.GameConfig;
+import ru.nsu.g.solovev5.m.task231.application.config.PlayerConfig;
+import ru.nsu.g.solovev5.m.task231.application.transport.SnakeRecord;
+import ru.nsu.g.solovev5.m.task231.domain.entities.Player;
+import ru.nsu.g.solovev5.m.task231.domain.entities.Snake;
+import ru.nsu.g.solovev5.m.task231.domain.valueobjects.Point2D;
+
+class GameWorkerTest {
+    final static GameConfig CONFIG = new GameConfig(
+        12, 15,
+        5,
+        List.of(
+            new PlayerConfig(
+                new Point2D(0, 0),
+                head -> head
+            )
+        )
+    );
+
+    GameWorker worker;
+
+    @BeforeEach
+    void setUp() {
+        worker = new GameWorker(
+            new CreateGameStateFromConfigUseCase(),
+            null,
+            CONFIG
+        );
+    }
+
+    @Test
+    void getGameState_should_returnStateEqualToConfig_when_noTicksDone() {
+        var state = worker.getGameState();
+
+        assertEquals(
+            CONFIG.rows(),
+            state.rows()
+        );
+        assertEquals(
+            CONFIG.columns(),
+            state.columns()
+        );
+        assertEquals(
+            CONFIG.maxFoodItems(),
+            state.maxFoodItems()
+        );
+        assertEquals(
+            CONFIG.players().stream()
+                .map(PlayerConfig::snakeHead)
+                .toList(),
+            state.players().stream()
+                .map(Player::snake)
+                .map(Snake::getHead)
+                .toList()
+        );
+    }
+
+    @Test
+    void getStateRecord_should_returnRecordEqualToConfig_when_noTicksDone() {
+        var state = worker.getStateRecord();
+
+        assertEquals(
+            CONFIG.rows(),
+            state.rows()
+        );
+        assertEquals(
+            CONFIG.columns(),
+            state.columns()
+        );
+        assertEquals(0, state.foods().size());
+        assertEquals(
+            CONFIG.players().stream()
+                .map(PlayerConfig::snakeHead)
+                .toList(),
+            state.snakes().stream()
+                .map(SnakeRecord::head)
+                .toList()
+        );
+    }
+}
