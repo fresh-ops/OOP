0131422c9018f718474c8779a9a2dcce986b74b8
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-04-25 00:22:24 +0700

    add: config loading

---
+++ Task_2_4_1/checker.groovy
@@ -0,0 +1,18 @@
+include "test.groovy"
+
+studyGroup("24213") {
+    student "Matvey" aka "fresh" submitsAt "https://github.com/fresh-ops/OOP"
+}
+
+studyGroup("24214") {
+    student "Matvey" aka "fresh" submitsAt "https://github.com/fresh-ops/OOP"
+}
+
+studyGroup("24215") {
+    student "Matvey" aka "fresh" submitsAt "https://github.com/fresh-ops/OOP"
+    student "Ruslana" aka "Rasymptote" submitsAt "https://github.com/Rasymptote/OOP"
+}
+
+studyGroup("24216") {
+    student "Matvey" aka "fresh" submitsAt "https://github.com/fresh-ops/OOP"
+}
---
+++ Task_2_4_1/src/main/groovy/ru/nsu/g/solovev5/m/task241/dsl/ConfigLoader.groovy
@@ -0,0 +1,36 @@
+package ru.nsu.g.solovev5.m.task241.dsl
+
+import ru.nsu.g.solovev5.m.task241.core.models.Config
+import ru.nsu.g.solovev5.m.task241.dsl.delegates.Statement
+import ru.nsu.g.solovev5.m.task241.dsl.delegates.StudyGroupBlock
+
+import java.nio.file.Path
+
+class ConfigLoader {
+    private final List<Statement> SUPPORTED_STATEMENTS = [
+            new StudyGroupBlock()
+    ]
+
+    Config loadFrom(Path path) {
+        var context = new ExecutionContext(SUPPORTED_STATEMENTS)
+        context.putExecuting(path)
+        context.binding().setVariable("include", {
+            String file -> this.include(Path.of(file), context)
+        })
+        var shell = new GroovyShell(context.binding())
+        shell.evaluate(path.toFile())
+        context.popExecuting(path)
+
+        context.config()
+    }
+
+    private void include(Path path, ExecutionContext context) {
+        if (context.isExecuting(path)) {
+            throw new RuntimeException("Circular includes on $path")
+        }
+        context.putExecuting(path)
+        var shell = new GroovyShell(context.binding())
+        shell.evaluate(path.toFile())
+        context.popExecuting(path)
+    }
+}
